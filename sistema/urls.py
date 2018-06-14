082d0adac9792b1d87556e5df3f4bd31c3fd7c4b
Leonardo Mello Silvestri <Leonardomscontato@gmail.com>
2018-06-14 16:57:33 -0300

    Adicionando rota do Django Authentication

--- sistema/urls.py
+++ sistema/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import home, profile, ranking, play, sign_up, sign_in, forgetPassword
+from .views import home, profile, ranking, play, register, forgetPassword
 
 urlpatterns = [
 	path('', home, name='url_home'),
 	path('profile/', profile, name='url_profile'),
 	path('ranking/', ranking, name='url_ranking'),
 	path('play/', play, name='url_play'),
-	path('sign_up/', sign_up, name='url_signup'),
-	path('sign_in/', sign_in, name='url_signin'),
+	path('accounts/register/', register, name='url_register'),
 	path('forget/', forgetPassword, name='url_forgetPassword'),
 ]
