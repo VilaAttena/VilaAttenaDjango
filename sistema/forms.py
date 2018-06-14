589e051907d8c1913e6256208faf2957f1d0f231
Leonardo Mello Silvestri <Leonardomscontato@gmail.com>
2018-06-14 16:55:20 -0300

    Adicionando formulários de Login e Cadastro do Django Authentication

--- sistema/forms.py
+++ sistema/forms.py
@@ -1,93 +1,55 @@
-from django.forms import ModelForm
+from django.forms import EmailField
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
-from .models import User
 
-class NewUserForm(ModelForm):
-	confirmPassword = forms.CharField(label='Confirmar Senha', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirme sua senha'}))	
-	class Meta:
-		model = User
-		fields = '__all__'
-		labels = {
-			'name': 'Nome',
-			'login': 'Nome de Usuário',
-			'email': 'E-mail',
-			'dateBirth': 'Data de Nascimento',
-			'password': 'Senha',			
-		}		
-		widgets = { 
-    	'name': forms.TextInput(attrs={'class': 'form-control',	'placeholder': 'Informe seu nome'}),
-    	'login': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Informe seu nome de usuário'}),
-    	'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Informe seu e-mail'}),
-    	'dateBirth': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
-    	'password': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Informe sua senha'}),     	   
-    }
-	
-	def clean_login(self):
-		login = self.cleaned_data['login']
-		try:
-			match = User.objects.get(login = login)
-		except:
-			return self.cleaned_data['login'] 
-		raise forms.ValidationError('Esse Nome de Usuário já está sendo usado!')
 
-	def clean_email(self):
-		email = self.cleaned_data['email']
-		try:
-			match = User.objects.get(email = email)
-		except:
-			return self.cleaned_data['email']
-		raise forms.ValidationError('Esse E-mail já está cadastrado!')
+class UserCreationForm(UserCreationForm):
+    email = forms.EmailField(label='Email', required=True) 
+    firstName = forms.CharField(label='Nome', required=True)
+    lastName = forms.CharField(label='Sobrenome', required=True)
 
-	def clean_confirmPassword(self):
-		password = self.cleaned_data['password']
-		confirmPassword = self.cleaned_data['confirmPassword']
-		MIN_LENGTH = 8
-		if password and confirmPassword:
-			if password != confirmPassword:
-				raise forms.ValidationError('As senhas não coincidem!')
-			else:
-				if len(password) < MIN_LENGTH:
-					raise forms.ValidationError('A senha deve conter pelo menos 8 carateres!')
-				if password.isdigit():
-					raise forms.ValidationError('A senha não pode contar apenas números!')
+    class Meta:
+        model = User
+        fields = ("firstName", "lastName", "username", "email", "password1", "password2")
 
-class LoginForm(ModelForm):
-	class Meta:
-		model = User
-		fields = ['login', 'password']
-		labels = {
-			'login': 'Usuário',
-			'password': 'Senha',
-		}
-		widgets = {
-			'login': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome de usuário'}),
-			'password': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Senha'}),
-		}
 
-class ProfileForm(ModelForm):
-	class Meta:
-		model = User
-		fields = ['name', 'login', 'email', 'dateBirth']
-		labels = {
-			'name': 'Nome',
-			'login': 'Nome de Usuário',
-			'email': 'E-mail',
-			'dateBirth': 'Data de Nascimento',
-		}
-		widgets = {
-			'name': forms.TextInput(attrs={'class': 'form-control',}),
-			'login': forms.TextInput(attrs={'class': 'form-control',}),
-			'email': forms.EmailInput(attrs={'class': 'form-control',}),
-			'dateBirth': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
-		}
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.firstName = self.cleaned_data["firstName"] 
+        user.lastName = self.cleaned_data["lastName"] 
+        user.email = self.cleaned_data["email"]                
+        if commit:
+            user.save()
+        return user
 
-class ForgetPasswordForm(ModelForm):
-	class Meta:
-		model = User
-		fields = ['email']
-		labels = {
-			'email': 'E-mail',			
-		}
-		widgets = {
-			'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Informe seu email'}),
-		}
+    def clean_username(self):        
+        username = self.cleaned_data.get('username')
+        
+        try:
+            match = User.objects.get(username=username)
+        except User.DoesNotExist:            
+            return username        
+        raise forms.ValidationError('Esse Nome de Usuário já existe!')
+
+    def clean_email(self):        
+        email = self.cleaned_data.get('email')
+        
+        try:
+            match = User.objects.get(email=email)
+        except User.DoesNotExist:            
+            return email        
+        raise forms.ValidationError('Esse E-mail já está cadastrado!')
+
+    def clean_password(self):
+        password = self.cleaned_data['password1']
+        confirmPassword = self.cleaned_data['password2']
+        MIN_LENGTH = 8
+        if password and confirmPassword:
+            if password != confirmPassword:
+                raise forms.ValidationError('As senhas não coincidem!')
+            else:
+                if len(password) < MIN_LENGTH:
+                    raise forms.ValidationError('A senha deve conter pelo menos 8 carateres!')
+                if password.isdigit():
+                    raise forms.ValidationError('A senha não pode contar apenas números!')
