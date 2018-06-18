6250b5eeb26f9dba2bf6cafcd0d50ee0100327d8
Leonardo Mello Silvestri <Leonardomscontato@gmail.com>
2018-06-18 13:13:52 -0300

    Adicionando uma rota para o formulário customizado de Login

--- sistema/urls.py
+++ sistema/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import home, profile, ranking, play, register, forgetPassword
-from django.contrib.auth.views import logout_then_login
+from django.contrib.auth.views import logout_then_login, login
+from .forms import LoginForm
 
 urlpatterns = [
 	path('', home, name='url_home'),
@@ -8,6 +9,7 @@ urlpatterns = [
 	path('ranking/', ranking, name='url_ranking'),
 	path('play/', play, name='url_play'),
 	path('accounts/register/', register, name='url_register'),
+	path('accounts/login/', login, {'authentication_form': LoginForm}),
 	path('forget/', forgetPassword, name='url_forgetPassword'),
 	path('accounts/logout/', lambda request: logout_then_login(request, "/"), name='url_logout'),
 ]
