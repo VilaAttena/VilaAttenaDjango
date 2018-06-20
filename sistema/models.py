79abe90bc019fb22cc8ee8c17cf19491cd69d587
LeonardoMelloSilvestri <Leonardomscontato@gmail.com>
2018-06-20 18:56:00 -0300

    Removendo códigos desnecessários

--- sistema/models.py
+++ sistema/models.py
@@ -1,13 +1,3 @@
 from django.db import models
 
 
-class User(models.Model):
-	name = models.CharField(max_length=100)
-	login = models.CharField(max_length=20)
-	email = models.EmailField(max_length=200)
-	dateBirth = models.DateField(auto_now=False, auto_now_add=False)
-	password = models.CharField(max_length=100)	
-
-	def __str__(self):
-		return self.login
-
