9e4d2b5904ffd744f8e4f9bd0819c6cdc0110d43
Leonardo Mello Silvestri <Leonardomscontato@gmail.com>
2018-06-22 12:50:20 -0300

    Criando um perfil para todo usuário cadastrado no sistema

--- sistema/models.py
+++ sistema/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthdate = models.DateField(null=True, blank=False)  
+
+    def __str__(self):
+        return self.user.username
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
