c57e54f37e898364851419f5e6b9180afda57e24
Leonardo Mello Silvestri <Leonardomscontato@gmail.com>
2018-06-22 12:56:00 -0300

    Adicionando rota para a view de editar perfil

--- sistema/urls.py
+++ sistema/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import home, profile, ranking, play, register, forgetPassword
+from .views import home, profile, edit_profile, ranking, play, register, forgetPassword
 from django.contrib.auth.views import logout_then_login, login
 from .forms import LoginForm
 
 urlpatterns = [
 	path('', home, name='url_home'),
 	path('profile/', profile, name='url_profile'),
+	path('edit_profile/', edit_profile, name='url_edit_profile'),
 	path('ranking/', ranking, name='url_ranking'),
 	path('play/', play, name='url_play'),
 	path('accounts/register/', register, name='url_register'),
