82f564ff4feaecda5966c3e027494fdaf4471113
Leonardo Mello Silvestri <Leonardomscontato@gmail.com>
2018-06-23 19:18:17 -0300

    Indicando que a pasta de templates servirá como fonte de templates substitutos para o Django

--- VilaAttena/settings.py
+++ VilaAttena/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'VilaAttena.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['sistema/static/sistema/base'],
+        'DIRS': ['sistema/static/sistema/base', 'sistema/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
