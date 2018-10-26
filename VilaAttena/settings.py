c11dd97a7f0ddf3beaf17f1f890e26ca45ddbde2
Leonardo Silvestri <leonardomscontato@gmail.com>
2018-10-26 13:35:49 -0300

    changes

--- VilaAttena/settings.py
+++ VilaAttena/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,4 +136,3 @@ EMAIL_HOST_PASSWORD = 'estudantesenai'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = 1
 
-django_heroku.settings(locals())
